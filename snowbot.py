9ebe7f89d36e4fb52cd55e1e56693f3c9953a00f
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2020-08-20 04:56:03 -0400

    added time converter command

--- snowbot.py
+++ snowbot.py
@@ -2,10 +2,12 @@ import discord
 from discord.ext import commands
 import random
 import os
-from datetime import datetime
+from platform import python_version
+import csv
+import datetime
 
 description = '''utility bot'''
-bot = commands.Bot(command_prefix='!', description=description)
+bot = commands.Bot(command_prefix='%', description=description)
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
 
@@ -14,6 +16,7 @@ async def on_ready():
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
+    print(f'Running python version {python_version()}')
     print('------')
 
 
@@ -33,7 +36,64 @@ async def roll(ctx, dice: str):
 @bot.command()
 async def members(ctx):
     """Returns number of members in the server, not including bots"""
+    # get the time given in UTC
+    with open('timezones.csv', newline='') as csvfile:
+        timezones = csv.reader(csvfile, delimiter=',')
+        utc_time = get_utc_time(timezones)
+        converted_time = utc_time
+
+    # get the time difference of the target to UTC
+    with open('timezones.csv', newline='') as csvfile:
+        timezones = csv.reader(csvfile, delimiter=',')
+        split_time = get_target_time_dif(timezones)
+        # split utc time zone to hours and minutes in case of time zones with minutes
+        time_dif_h = float(split_time[0])
+        time_dif_m = 0
+        # check if there was a split before trying to get minutes
+        if len(split_time) is 2:
+            time_dif_m = float(split_time[1])
+
+        # apply timezone time difference
+        converted_time = converted_time + datetime.timedelta(hours=time_dif_h, minutes=time_dif_m)
+
+    def get_utc_time(timezones):
+        for row in timezones:
+            # check for timezone argument against csv data
+            if row[0].lower() == sys.argv[2].lower():
+                utc_timezone = row[2]
+                utc_time_dif = utc_timezone[3:]
+                entered_time = datetime.datetime.strptime(sys.argv[1], '%H.%M')
+                # split utc time zone to hours and minutes in case of time zones with minutes
+                split_time = utc_time_dif.split('.')
+                time_dif_h = 0 - float(split_time[0])
+                time_dif_m = 0
+                # check if there was a split before trying to get minutes
+                if len(split_time) is 2:
+                    time_dif_m = float(split_time[1])
+
+                # apply timezone time difference
+                utc_time = entered_time + datetime.timedelta(hours=time_dif_h, minutes=time_dif_m)
+                return utc_time
+        # if it get's here timezone code was wrong
+        print('First timezone not found')
+
+    def get_target_time_dif(timezones):
+        # check for timezone argument against csv data
+        for row in timezones:
+            if row[0].lower() == sys.argv[3].lower():
+                utc_timezone = row[2]
+                utc_time_dif = utc_timezone[3:]
+                split_time = utc_time_dif.split('.')
+                return split_time
+        # if it get's here timezone code was wrong
+        print('Second timezone not found')
+
     await ctx.send(f'member count: {len([m for m in ctx.guild.members if not m.bot])}')
+
+
+@bot.command()
+async def time(ctx, time: str, src_zone: str, dest_zone: str):
+    await ctx.send(f'{time} {src_zone} is {converted_time} {dest_zone}')
 
 
 # @bot.command()
