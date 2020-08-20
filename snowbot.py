5361426cbf7c2abbf35215b91450098e234fde15
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2020-08-20 04:16:21 -0400

    added member count command

--- snowbot.py
+++ snowbot.py
@@ -2,11 +2,10 @@ import discord
 from discord.ext import commands
 import random
 import os
+from datetime import datetime
 
-description = '''An example bot to showcase the discord.ext.commands extension
-module.
-There are a number of utility commands being showcased here.'''
-bot = commands.Bot(command_prefix='?', description=description)
+description = '''utility bot'''
+bot = commands.Bot(command_prefix='!', description=description)
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
 
@@ -16,12 +15,6 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-
-
-@bot.command()
-async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left + right)
 
 
 @bot.command()
@@ -37,37 +30,49 @@ async def roll(ctx, dice: str):
     await ctx.send(result)
 
 
-@bot.command(description='For when you wanna settle the score some other way')
-async def choose(ctx, *choices: str):
-    """Chooses between multiple choices."""
-    await ctx.send(random.choice(choices))
-
-
 @bot.command()
-async def repeat(ctx, times: int, content='repeating...'):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await ctx.send(content)
+async def members(ctx):
+    """Returns number of members in the server, not including bots"""
+    await ctx.send(f'member count: {len([m for m in ctx.guild.members if not m.bot])}')
 
 
-@bot.command()
-async def joined(ctx, member: discord.Member):
-    """Says when a member joined."""
-    await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
+# @bot.command()
+# async def add(ctx, left: int, right: int):
+#     """Adds two numbers together."""
+#     await ctx.send(left + right)
 
 
-@bot.group()
-async def cool(ctx):
-    """Says if a user is cool.
-    In reality this just checks if a subcommand is being invoked.
-    """
-    if ctx.invoked_subcommand is None:
-        await ctx.send('No, {0.subcommand_passed} is not cool'.format(ctx))
+# @bot.command(description='For when you wanna settle the score some other way')
+# async def choose(ctx, *choices: str):
+#     """Chooses between multiple choices."""
+#     await ctx.send(random.choice(choices))
 
 
-@cool.command(name='bot')
-async def _bot(ctx):
-    """Is the bot cool?"""
-    await ctx.send('Yes, the bot is cool.')
+# @bot.command()
+# async def repeat(ctx, times: int, content='repeating...'):
+#     """Repeats a message multiple times."""
+#     for i in range(times):
+#         await ctx.send(content)
+
+
+# @bot.command()
+# async def joined(ctx, member: discord.Member):
+#     """Says when a member joined."""
+#     await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
+
+
+# @bot.group()
+# async def cool(ctx):
+#     """Says if a user is cool.
+#     In reality this just checks if a subcommand is being invoked.
+#     """
+#     if ctx.invoked_subcommand is None:
+#         await ctx.send('No, {0.subcommand_passed} is not cool'.format(ctx))
+
+
+# @cool.command(name='bot')
+# async def _bot(ctx):
+#     """Is the bot cool?"""
+#     await ctx.send('Yes, the bot is cool.')
 
 bot.run(BOT_TOKEN)
