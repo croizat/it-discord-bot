c650e28c9069d9afdffc61c97b9f739fd24cb4ea
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2020-08-20 02:36:41 -0400

    bot token var fix

--- snowbot.py
+++ snowbot.py
@@ -1,12 +1,13 @@
 import discord
 from discord.ext import commands
 import random
+import os
 
 description = '''An example bot to showcase the discord.ext.commands extension
 module.
 There are a number of utility commands being showcased here.'''
 bot = commands.Bot(command_prefix='?', description=description)
-
+os.environ.get('BOT_TOKEN')
 
 @bot.event
 async def on_ready():
