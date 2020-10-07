1a928288e060f85cf251691ad45004baeac66a7c
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2020-10-07 00:00:24 -0400

    t

--- snowbot.py
+++ snowbot.py
@@ -7,7 +7,7 @@ import sys
 import os
 from platform import python_version
 
-import cogs.utilities
+import utilities
 
 description = '''random utilities and shitposting bot'''
 bot = commands.Bot(command_prefix='$', case_insensitive=True, description=description)
