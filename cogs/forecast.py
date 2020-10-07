e2e89fa5c297af8baf4210b7ba874920556c9c9f
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2020-10-06 22:54:41 -0400

    weather

---
+++ cogs/forecast.py
@@ -0,0 +1,38 @@
+import discord
+import requests
+from discord.ext import commands
+import weather
+
+
+class Weather(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+
+
+    @commands.command()
+    async def weather(self, context, *args):
+        if not args:
+            forecast = weather.getForecast('victoria, sc')
+            embed = discord.Embed(title='Weather Forecast')
+
+            embed.add_field(name=forecast.name, value='Temperature: ' + forecast.temperature +
+                            '\n' + 'Description: ' + forecast.description, inline=True)
+
+            await context.send(embed=embed)
+        else:
+            try:
+                string = ' '.join(args)
+                forecast = weather.getForecast(string)
+                embed = discord.Embed(title='Weather Forecast')
+
+                embed.add_field(name=forecast.name, value='Temperature: ' + forecast.temperature +
+                                '\n' + 'Description: ' + forecast.description, inline=True)
+
+                await context.send(embed=embed)
+            except:
+                await context.send(""""$weather <city>" dumbfuck. If you did that and it didn't work then the city wasn't count""")
+
+
+def setup(bot):
+    bot.add_cog(Weather(bot))
--- cogs/shitposting.py
+++ cogs/shitposting.py
@@ -46,21 +46,8 @@ class Shitposts(commands.Cog):
             for line in output:
                 await message.channel.send(line)
         if "epstein" in message.content.lower():
-            output = ["""JOSH: Drake... where's Epstein?""",
-                      """DRAKE: He’s right in there. See, I put him on suicide watch.""",
-                      """[laugh track]""",
-                      """JOSH: You were supposed to monitor him constantly.""",
-                      """DRAKE: Dude, I'm gonna!""",
-                      """JOSH: Oh really?""",
-                      """DRAKE: Yes!""",
-                      """JOSH: So go check on him!""",
-                      """DRAKE: Okay, I will! tcch""",
-                      """[laugh track]""",
-                      """[rattles the door, peeps through the door, turns around]""",
-                      """DRAKE: ...I see the problem.""",
-                      """JOSH: Oh do ya?"""]
-            for line in output:
-                await message.channel.send(line)
+            output = "JOSH: Drake... where's Epstein?\nDRAKE: He’s right in there. See, I put him on suicide watch.\n[laugh track]\nJOSH: You were supposed to monitor him constantly.\nDRAKE: Dude, I'm gonna!\nJOSH: Oh really?\nDRAKE: Yes!\nJOSH: So go check on him!\nDRAKE: Okay, I will! tcch\n[laugh track]\n[rattles the door, peeps through the door, turns around]\nDRAKE: ...I see the problem.\nJOSH: Oh do ya?"
+            await message.channel.send(output)
         if "drugs" in message.content.lower():
             output = """We should not be arrested for drugs if we are just vibing. If we are just vibing, it's our vibe, drugs a vibe so we should be able to vibe, it's part of our identity"""
             await message.channel.send(output)
@@ -90,6 +77,12 @@ class Shitposts(commands.Cog):
             await message.channel.send(output)
         if "ussr" in message.content.lower():
             output = """> ussr\n19 August 1991 worst day of my life"""
+            await message.channel.send(output)
+        if "rawr" in message.content.lower():
+            output = """x3 nuzzles how are you pounces on you you're so warm o3o notices you have a bulge o: someone's happy ;) nuzzles your necky wecky~ murr~ hehehe rubbies your bulgy wolgy you're so big :oooo rubbies more on your bulgy wolgy it doesn't stop growing ·///· kisses you and lickies your necky daddy likies (; nuzzles wuzzles I hope daddy really likes $: wiggles butt and squirms I want to see your big daddy meat~ wiggles butt I have a little itch o3o wags tail can you please get my itch~ puts paws on your chest nyea~ its a seven inch itch rubs your chest can you help me pwease squirms pwetty pwease sad face I need to be punished runs paws down your chest and bites lip like I need to be punished really good~ paws on your bulge as I lick my lips I'm getting thirsty. I can go for some milk unbuttons your pants as my eyes glow you smell so musky :v licks shaft mmmm~ so musky drools all over your cock your daddy meat I like fondles Mr. Fuzzy Balls hehe puts snout on balls and inhales deeply oh god im so hard~ licks balls punish me daddy~ nyea~ squirms more and wiggles butt I love your musky goodness bites lip please punish me licks lips nyea~ suckles on your tip so good licks pre of your cock salty goodness~ eyes role back and goes balls deep mmmm~ moans and suckles"""
+            await message.channel.send(output)
+        if "dad" in message.content.lower():
+            output = """> dad\ndaddy*"""
             await message.channel.send(output)
 
 
