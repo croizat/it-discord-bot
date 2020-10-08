32177fe67c4b4d3cc265377d780f32cfd6007b23
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2020-10-08 00:39:31 -0400

    a

---
+++ cogs/utilities.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+import random
+
+
+class Utilities(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+
+     @commands.command()
+    async def roll(ctx, dice: str):
+        """Rolls a dice in NdN format."""
+        try:
+            rolls, limit = map(int, dice.split('d'))
+        except Exception:
+            await ctx.send('Format has to be in NdN!')
+            return
+
+        result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+        await ctx.send(result)
+
+     @commands.command()
+    async def choose(ctx, *choices: str):
+        """Chooses between multiple choices."""
+        await ctx.send(random.choice(choices))
+
+
+def setup(bot):
+    bot.add_cog(Utilities(bot))
