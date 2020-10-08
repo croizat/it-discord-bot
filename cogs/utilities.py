58ada315b82ea2ea090622e358c2dc9273e5cb98
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2020-10-08 00:56:00 -0400

    a

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import random
+import urbandict
 
 
 class Utilities(commands.Cog):
@@ -14,16 +15,39 @@ class Utilities(commands.Cog):
         try:
             rolls, limit = map(int, dice.split('d'))
         except Exception:
-            await ctx.send('Format has to be in NdN!')
+            await ctx.channel.send('Format has to be in NdN!')
             return
 
         result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-        await ctx.send(result)
+        await ctx.channel.send(result)
 
     @commands.command()
     async def choose(ctx, *choices: str):
         """Chooses between multiple choices."""
-        await ctx.send(random.choice(choices))
+        await ctx.channel.send(random.choice(choices))
+
+    @commands.command()
+    async def aesthetify(self, ctx, *, text: str):
+        """Returns inputed text in aesthetics"""
+        final = ""
+        pre = ' '.join(text)
+        for char in pre:
+            if not ord(char) in range(33, 127):
+                final += char
+                continue
+            final += chr(ord(char) + 65248)
+        await self.truncate(ctx.message.channel, final)
+
+    @commands.command()
+    async def urban(self, ctx, *, word: str):
+        urb = urbandict.define(word)
+        if "There aren't any definitions" in urb[0]['def']:
+            await self.bot.say(":no_mouth: `No definition found.`")
+            return
+        msg = "**{0}**\n".format(word)
+        msg += "`Definition:` {0}\n".format(urb[0]['def'].replace("\n", ""))
+        msg += "`Example:` {0}".format(urb[0]['example'].replace("\n", ""))
+        await self.truncate(ctx.message.channel, msg)
 
 
 def setup(bot):
