4f74c70e6d372a76c9475bec7ab59de0509d5d5a
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2020-10-20 18:48:44 -0400

    .

--- cogs/shitposting.py
+++ cogs/shitposting.py
@@ -91,24 +91,23 @@ class Shitposts(commands.Cog):
             return None
         for key in self.posts:
             val = self.posts.get(key)
-            output = None
             link = None
             if re.search(key, message.content.lower()):
                 output = val
                 if isinstance(type(val), tuple):
                     link = val[1]
-            if link is not None:
-                picture = discord.Embed()
-                picture.set_image(url=link)
-                await message.channel.send(content=output, embed=picture)
-            elif isinstance(type(output), list):
-                if output[-1] == "random":
-                    await message.channel.send(random.choice(output[:-1]))
+                if link is not None:
+                    picture = discord.Embed()
+                    picture.set_image(url=link)
+                    await message.channel.send(content=output, embed=picture)
+                elif isinstance(type(output), list):
+                    if output[-1] == "random":
+                        await message.channel.send(random.choice(output[:-1]))
+                    else:
+                        for line in output:
+                            await message.channel.send(line)
                 else:
-                    for line in output:
-                        await message.channel.send(line)
-            else:
-                await message.channel.send(output)
+                    await message.channel.send(output)
         # if re.search(r"\bthe senate\b", message.content.lower()):
         #     output = "**Did you ever hear the tragedy of Darth Plagueis the Wise? I thought not. It's not a story the Jedi would tell you. It's a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life... He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful... the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic, he could save others from death, but not himself.**"
         #     picture = discord.Embed()
