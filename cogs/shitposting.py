88de8edbd74141f50668f6bf2f47aab8a6aa1fd0
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2020-10-20 20:58:21 -0400

    prevent same shitpost twice in a row

--- cogs/shitposting.py
+++ cogs/shitposting.py
@@ -11,6 +11,7 @@ class Shitposts(commands.Cog):
         self.bot = bot
         self._last_member = None
         self.last_time_executed = datetime.utcfromtimestamp(0)
+        self.last_message = None
         self.posts = {
             r"\bthe senate\b": (
                 "**Did you ever hear the tragedy of Darth Plagueis the Wise? I thought not. It's not a story the Jedi would tell you. It's a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life... He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful... the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic, he could save others from death, but not himself.**",
@@ -101,7 +102,9 @@ class Shitposts(commands.Cog):
             for key in self.posts:
                 val = self.posts.get(key)
                 link = None
-                if re.search(key, message.content.lower()):
+                match = re.search(key, message.content.lower())
+                if match and match.group(0) != self.last_message:
+                    self.last_message = match.group(0)
                     output = val
                     if isinstance(output, tuple):
                         link = output[1]
